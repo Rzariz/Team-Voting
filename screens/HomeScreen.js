0ec78ca31493341d6eda6df0e8cdef43442832df
varsha <varshavasanthan366@gmail.com>
2022-04-11 23:30:27 +0530

    new style for home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,17 +66,17 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"coral",
+    backgroundColor:"lightblue",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
-    borderRadius: 15,
+    borderRadius: 20,
     margin:10,
     width: 150,
     height: 50,
   },
   ratingContainer: {
     alignSelf: 'center',
-    marginTop: 50,
+    marginTop: 40,
   },
 });
